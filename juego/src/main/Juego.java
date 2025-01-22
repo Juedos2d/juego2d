e8548127e3032d2c456509b7bb656aaff22fc178
yeray124 <yeray0451@gmail.com>
2025-01-22 18:54:44 +0100

    Creacion hilo principal

--- juego/src/main/Juego.java
+++ juego/src/main/Juego.java
@@ -1,14 +1,21 @@
 package main;
 
-public class Juego {
+public class Juego implements Runnable{
 	private VentanaJuego ventanaJuego;
 	private PanelJuego panelJuego;
+	private Thread juegoHilo;
 	
 	public Juego() {
 		panelJuego = new PanelJuego();
 		ventanaJuego = new VentanaJuego(panelJuego);
 		panelJuego.setFocusable(true);
 		panelJuego.requestFocus(); // Establece un foco de entrada para los eventos
+		
+	}
+	
+	
+	@Override
+	public void run() {
 		
 	}
 }
